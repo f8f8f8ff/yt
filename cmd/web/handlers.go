78dba90c2ff4020bd7ef208aafefb2ccc8c3056f
reed <reedhdonaldson@gmail.com>
2023-07-08 20:55:39 -0700

    allow downloading audio by replacing domain

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -8,7 +8,7 @@ import (
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		app.notFound(w)
+		app.download(w, r, r.URL.RequestURI(), "audio")
 		return
 	}
 	files := []string{
